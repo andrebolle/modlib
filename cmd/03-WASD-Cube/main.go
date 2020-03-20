e9b7c8c88ce6fd13c3a8b52e07c2e9df595d2337
Andre Bolle <andrebolle@gmail.com>
2020-03-20 14:07:49 +0000

    Removed crud

--- cmd/03-WASD-Cube/main.go
+++ cmd/03-WASD-Cube/main.go
@@ -20,59 +20,6 @@ func init() {
 	// GLFW event handling must run on the main OS thread
 	runtime.LockOSThread()
 }
-func init() {
-	// GLFW event handling must run on the main OS thread
-	fmt.Println("Init")
-}
-
-// func loadTexture(file string) (uint32, error) {
-// 	imgFile, err := os.Open(file)
-// 	if err != nil {
-// 		return 0, fmt.Errorf("texture %q not found on disk: %v", file, err)
-// 	}
-// 	img, formatName, err := image.Decode(imgFile)
-// 	if err != nil {
-// 		return 0, err
-// 	}
-
-// 	fmt.Println("Image format is", formatName)
-// 	fmt.Println("Image Bounds is", img.Bounds())
-
-// 	//              |
-// 	// +-------------------------+
-// 	// |                         |
-// 	// |    Convert the Image    |
-// 	// |                         |
-// 	// +-------------------------+
-// 	//              |
-
-// 	rgba := image.NewRGBA(img.Bounds())
-// 	if rgba.Stride != rgba.Rect.Size().X*4 {
-// 		return 0, fmt.Errorf("unsupported stride")
-// 	}
-// 	draw.Draw(rgba, rgba.Bounds(), img, image.Point{0, 0}, draw.Src)
-
-// 	var texture uint32
-// 	gl.GenTextures(1, &texture)
-// 	gl.ActiveTexture(gl.TEXTURE0)
-// 	gl.BindTexture(gl.TEXTURE_2D, texture)
-// 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
-// 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
-// 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
-// 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-// 	gl.TexImage2D(
-// 		gl.TEXTURE_2D,
-// 		0,
-// 		gl.RGBA,
-// 		int32(rgba.Rect.Size().X),
-// 		int32(rgba.Rect.Size().Y),
-// 		0,
-// 		gl.RGBA,
-// 		gl.UNSIGNED_BYTE,
-// 		gl.Ptr(rgba.Pix))
-
-// 	return texture, nil
-// }
 
 func main() {
 
