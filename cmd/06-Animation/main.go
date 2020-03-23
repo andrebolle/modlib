a65bf815d64c25cecef5238901e28c39e94245ee
Andre Bolle <andrebolle@gmail.com>
2020-03-23 09:53:18 +0000

    Tidy the code

--- cmd/06-Animation/main.go
+++ cmd/06-Animation/main.go
@@ -15,30 +15,11 @@ import (
 
 func main() {
 
-	// Wire this calling goroutine to its current operating system thread.
+	// Lock this calling goroutine to its current operating system thread.
 	runtime.LockOSThread()
 
+	// Print instructions
 	fmt.Println("Use W,A,S,D,R,F to move forward, left, backward, right, up and down.")
-
-	// // -------------------------Create the Vertices ------------------------
-
-	// //Hilbert https://en.wikipedia.org/wiki/Hilbert_curve#Representation_as_Lindenmayer_system
-	// var hilbert = utils.L{
-	// 	Seed:  "A",
-	// 	Angle: math.Pi / 2,
-	// 	Rules: map[rune]string{
-	// 		'A': "-BF+AFA+FB-",
-	// 		'B': "+AF-BFB-FA+",
-	// 		'F': "F",
-	// 		'-': "-",
-	// 		'+': "+"}}
-
-	// // Generate the points
-	// floatArray, coordCount := utils.Lsystem(hilbert, 6)
-	// fmt.Println("coordCount", coordCount)
-	// points := coordCount / 2
-
-	// ------------------------ Camera and Window  ----------------------------------
 
 	// Set the width of the window in pixels
 	const width float32 = 1600
@@ -53,8 +34,6 @@ func main() {
 
 	// Create a window
 	win := utils.CreateWindow(os.Args[0], int(width), int(height))
-
-	// ------------------------ Keyboard Input ----------------------------------
 
 	// Define the keyboard input callback function
 	keyCallback := func(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
@@ -75,51 +54,42 @@ func main() {
 		}
 	}
 
+	// Set Keyboard Callback function
 	win.SetKeyCallback(keyCallback)
 
-	// ------------------------ Load Shader ----------------------------------
-
-	// Create and install(Use) a shader
+	// Create and Use the Shader
 	program, _ := utils.CreateVF(utils.MVPVertShader, utils.MVPFragShader)
 	defer gl.DeleteProgram(program)
 	gl.UseProgram(program)
 
-	// --------------------------- Uniforms ----------------------------------
-
-	// Get Uniform locations
+	// Get Uniform and Atrribute Locations
 	modelUniform := gl.GetUniformLocation(program, gl.Str("model\x00"))
 	viewLocation := gl.GetUniformLocation(program, gl.Str("view\x00"))
 	projectionUniform := gl.GetUniformLocation(program, gl.Str("projection\x00"))
+	positionLocation := uint32(gl.GetAttribLocation(program, gl.Str("position\x00")))
 
 	// Set Model
 	model := mgl32.Ident4()
 	gl.UniformMatrix4fv(modelUniform, 1, false, &model[0])
 
-	// Update Projection
+	// Set Projection
 	projection := mgl32.Perspective(cam.Fovy, cam.Aspect, cam.Near, cam.Far)
 	gl.UniformMatrix4fv(projectionUniform, 1, false, &projection[0])
 
-	positionLocation := uint32(gl.GetAttribLocation(program, gl.Str("position\x00")))
-
-	// ------------------------ Create Vertex Array --------------------------
-
+	// Create a Vertex Array
 	var array uint32
-	// array -> vertex array
 	gl.GenVertexArrays(1, &array)
 	gl.BindVertexArray(array)
 
-	// ------------------------ Create Buffer --------------------------
-
+	// Create a Buffer
 	var buffer uint32
-	// buffer -> buffer
 	gl.GenBuffers(1, &buffer)
 	gl.BindBuffer(gl.ARRAY_BUFFER, buffer)
 
-	// ------------------------ Describe data Data --------------------------
-	// size: components per vertex attribute
+	// Describe the Position
+	// size: 2 floats per Position
 	gl.VertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, gl.PtrOffset(0))
 
-	// ------------------------ Enable this attributes
 	// Causes position to be passed to the shader
 	gl.EnableVertexAttribArray(positionLocation)
 
