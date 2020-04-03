7b3344de94087e8fc3e56546b67f81ee19825f6c
Andre Bolle <andrebolle@gmail.com>
2020-04-03 13:24:48 +0100

    added lsystem 3D drawn using g3n

---
+++ cmd/lsysg3n/lsys/lsys.go
@@ -0,0 +1,125 @@
+package lsys
+
+import (
+	"github.com/go-gl/mathgl/mgl32"
+)
+
+// L3D L-System
+type L3D struct {
+	Seed  string
+	Angle float64
+	Rules map[rune]string
+}
+
+//GenLString3D GenLString
+func GenLString3D(l L3D, count uint) (oldSeed string) {
+	oldSeed = l.Seed
+	for i := uint(0); i < count; i++ {
+		newSeed := ""
+		for _, n := range oldSeed {
+			newSeed += l.Rules[n]
+		}
+		oldSeed = newSeed
+	}
+	return
+}
+
+// Turtle3D {x,y,heading}
+type Turtle3D struct {
+	position          mgl32.Vec3
+	direction         mgl32.Vec3
+	right             mgl32.Vec3
+	heading, d, theta float64
+}
+
+const coordMax3D int = 50000
+
+//Lsystem3D Lsystem
+func Lsystem3D(lString string, angle float64) (*[coordMax3D]float32, int) {
+
+	const coordinates = 3
+
+	// Coord array
+	var floats [coordMax3D]float32
+
+	// Create a Turtle
+	t := Turtle3D{
+		position:  mgl32.Vec3{0, 0, 0},
+		direction: mgl32.Vec3{0, 1, 0},
+		right:     mgl32.Vec3{1, 0, 0},
+		heading:   0,
+		d:         0.1,
+		theta:     angle}
+
+	tally := 0
+
+	floats[0] = float32(t.position.X())
+	floats[1] = float32(t.position.Y())
+	floats[2] = float32(t.position.Z())
+
+	tally = coordinates
+
+	for i := 0; i < len(lString); i++ {
+
+		command := lString[i]
+
+		switch command {
+
+		// Draw
+		case 'F':
+			t.position = t.position.Add(t.direction.Mul(float32(t.d)))
+			floats[tally] = float32(t.position.X())
+			floats[tally+1] = float32(t.position.Y())
+			floats[tally+2] = float32(t.position.Z())
+			tally += coordinates
+
+		// Yaw/Turn
+		case '+':
+			up := t.direction.Cross(t.right)
+			quatRotate := mgl32.QuatRotate(float32(angle), up)
+			t.direction = quatRotate.Rotate(t.direction)
+			t.right = quatRotate.Rotate(t.right)
+			t.direction = t.direction.Normalize()
+			t.right = t.right.Normalize()
+
+		case '-':
+			up := t.direction.Cross(t.right)
+			quatRotate := mgl32.QuatRotate(float32(-angle), up)
+			t.direction = quatRotate.Rotate(t.direction)
+			t.right = quatRotate.Rotate(t.right)
+			t.direction = t.direction.Normalize()
+			t.right = t.right.Normalize()
+
+		// Pitch
+		case '^':
+			quatRotate := mgl32.QuatRotate(float32(-angle), t.right)
+			t.direction = quatRotate.Rotate(t.direction)
+			t.direction = t.direction.Normalize()
+
+		case '&':
+			quatRotate := mgl32.QuatRotate(float32(angle), t.right)
+			t.direction = quatRotate.Rotate(t.direction)
+			t.direction = t.direction.Normalize()
+
+		// Roll
+		case '>':
+			quatRotate := mgl32.QuatRotate(float32(angle), t.direction)
+			t.right = quatRotate.Rotate(t.right)
+			t.right = t.right.Normalize()
+
+		case '<':
+			quatRotate := mgl32.QuatRotate(float32(-angle), t.direction)
+			t.right = quatRotate.Rotate(t.right)
+			t.right = t.right.Normalize()
+
+		case 'A':
+		case 'B':
+		case 'X':
+		default:
+			panic("unrecognized character")
+		}
+
+	}
+
+	return &floats, tally
+}
---
+++ cmd/lsysg3n/main.go
@@ -0,0 +1,125 @@
+package main
+
+// Run this ...
+// go get github.com/g3n/engine@master
+
+import (
+	"fmt"
+	"math"
+	"time"
+
+	"github.com/g3n/engine/app"
+	"github.com/g3n/engine/camera"
+	"github.com/g3n/engine/core"
+	"github.com/g3n/engine/geometry"
+	"github.com/g3n/engine/gls"
+	"github.com/g3n/engine/graphic"
+	"github.com/g3n/engine/gui"
+	"github.com/g3n/engine/light"
+	"github.com/g3n/engine/material"
+	"github.com/g3n/engine/math32"
+	"github.com/g3n/engine/renderer"
+	"github.com/g3n/engine/util/helper"
+	"github.com/g3n/engine/window"
+	"github.com/purelazy/modlib/cmd/lsysg3n/lsys"
+)
+
+func main() {
+
+	var hilbert = lsys.L3D{
+		Seed:  "X",
+		Angle: math.Pi / 2,
+		Rules: map[rune]string{
+			'X': "^<XF^<XFX-F^>>XFX&F+>>XFX-F>X->",
+			'F': "F",
+			'-': "-",
+			'+': "+",
+			'^': "^",
+			'&': "&",
+			'<': "<",
+			'>': ">",
+		}}
+
+	// Generate the L-system string
+	snowflake := lsys.GenLString3D(hilbert, 3)
+	fmt.Println(snowflake)
+
+	// Create first frame
+	floatArray, coordCount := lsys.Lsystem3D(snowflake, hilbert.Angle)
+	points := coordCount / 3
+	fmt.Println(floatArray[0], points)
+
+	// Convert coordinates to slice of math32.Vector3s
+	vector3s := make([]math32.Vector3, 0, 50000)
+	for p := 0; p < coordCount; p += 3 {
+		vector3s = append(vector3s, math32.Vector3{X: floatArray[p], Y: floatArray[p+1], Z: floatArray[p+2]})
+	}
+
+	// --------------------------------
+
+	// App returns the Application singleton, creating it the first time.
+	a := app.App()
+
+	// NewNode returns a pointer to a new Node.
+	scene := core.NewNode()
+
+	// Set the scene to be managed by the gui manager
+	gui.Manager().Set(scene)
+
+	// Create perspective camera
+	cam := camera.New(1)
+	cam.SetPosition(0, 0, 3)
+	scene.Add(cam)
+
+	// Set up orbit control for the camera
+	camera.NewOrbitControl(cam)
+
+	// Set up callback to update viewport and camera aspect ratio when the window is resized
+	onResize := func(evname string, ev interface{}) {
+		// Get framebuffer size and update viewport accordingly
+		width, height := a.GetSize()
+		a.Gls().Viewport(0, 0, int32(width), int32(height))
+		// Update the camera's aspect ratio
+		cam.SetAspect(float32(width) / float32(height))
+	}
+	a.Subscribe(window.OnWindowSize, onResize)
+	onResize("", nil)
+
+	// Create a blue torus and add it to the scene
+	geom := geometry.NewTube(vector3s, 0.01, 12, false)
+	//geom := geometry.NewTube([]math32.Vector3{{X: 0, Y: 0, Z: 0}, {X: 0.5, Y: 0, Z: 0}, {X: 0.5, Y: 0.5, Z: 0}}, 0.2, 12, false)
+	// geom := NewTube(path []math32.Vector3, radius float32, radialSegments int, close bool)
+	//geom := geometry.NewTorus(1, .4, 12, 32, math32.Pi*2)
+	mat := material.NewStandard(math32.NewColor("DarkBlue"))
+	mesh := graphic.NewMesh(geom, mat)
+	scene.Add(mesh)
+
+	// Create and add a button to the scene
+	btn := gui.NewButton("Make Red")
+	btn.SetPosition(100, 40)
+	btn.SetSize(40, 40)
+	btn.Subscribe(gui.OnClick, func(name string, ev interface{}) {
+		mat.SetColor(math32.NewColor("DarkRed"))
+	})
+	scene.Add(btn)
+
+	// Create and add lights to the scene
+	scene.Add(light.NewAmbient(&math32.Color{R: 1.0, G: 1.0, B: 1.0}, 0.8))
+	pointLight := light.NewPoint(&math32.Color{R: 1, G: 1, B: 1}, 5.0)
+	pointLight.SetPosition(1, 0, 2)
+	scene.Add(pointLight)
+
+	// Create and add an axis helper to the scene
+	scene.Add(helper.NewAxes(0.5))
+
+	// Set background color to gray
+	a.Gls().ClearColor(0.5, 0.5, 0.5, 1.0)
+
+	// Run the application
+	a.Run(func(renderer *renderer.Renderer, deltaTime time.Duration) {
+		a.Gls().FrontFace(gls.CCW)
+		a.Gls().CullFace(gls.FRONT)
+		a.Gls().Clear(gls.DEPTH_BUFFER_BIT | gls.STENCIL_BUFFER_BIT | gls.COLOR_BUFFER_BIT)
+		renderer.Render(scene, cam)
+	})
+}
