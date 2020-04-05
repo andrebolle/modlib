3ff331adffb9c3f1235c10dc9a4af42fbf365415
Andre Bolle <andrebolle@gmail.com>
2020-04-05 05:42:26 +0100

    Tidying Up

--- cmd/00-Cube/main.go
+++ cmd/00-Cube/main.go
@@ -11,57 +11,96 @@ import (
 
 func main() {
 
-	// ---------------------- Setup
-
-	// A basic Window and Camera
+	// Window, Camera
 	win, cam := utils.GetWindowAndCamera()
 	defer win.Destroy()
 
-	// A basic Shader
+	// Program
 	program, _ := utils.CreateVF(utils.ReadShader("mvp.vs.glsl"), utils.ReadShader("mvp.fs.glsl"))
 	defer gl.DeleteProgram(program)
 	gl.UseProgram(program)
 
-	// A basic Vertex Array and Buffer
-	var array, buffer uint32
-	gl.GenVertexArrays(1, &array)
-	gl.BindVertexArray(array)
-	gl.GenBuffers(1, &buffer)
-	gl.BindBuffer(gl.ARRAY_BUFFER, buffer)
-
-	// The array of Vertices
-	var vertices = [...]mgl32.Vec2{
-		// Triangle 1
-		{-0.90, -0.90},
-		{0.85, -0.90},
-		{-0.90, 0.85},
-		// Triangle 2
-		{0.90, -0.85},
-		{0.90, 0.90},
-		{-0.85, 0.90},
-	}
-
-	// Copy the Vertices to the Buffer
-	gl.BufferData(gl.ARRAY_BUFFER, int(unsafe.Sizeof(vertices)), unsafe.Pointer(&vertices), gl.STATIC_DRAW)
-
-	// Get Uniform and Atrribute Locations
+	// Get Locations
 	modelLocation := gl.GetUniformLocation(program, gl.Str("model\x00"))
 	viewLocation := gl.GetUniformLocation(program, gl.Str("view\x00"))
 	projectionLocation := gl.GetUniformLocation(program, gl.Str("projection\x00"))
 	positionLocation := uint32(gl.GetAttribLocation(program, gl.Str("pos\x00")))
 
-	// Set VertexAttribPointer & EnableVertexAttribArray
-	gl.VertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, gl.PtrOffset(0))
-	gl.EnableVertexAttribArray(positionLocation)
-
-	// Use UniformMatrix4fv to set MVP as required
+	// Bind Locations
 	model := mgl32.Ident4()
 	gl.UniformMatrix4fv(modelLocation, 1, false, &model[0])
 	projection := mgl32.Perspective(cam.Fovy, cam.Aspect, cam.Near, cam.Far)
 	gl.UniformMatrix4fv(projectionLocation, 1, false, &projection[0])
 
+	// The Vertices
+	// The array of Vertices
+	var cube = []float32{
+		// Bottom
+		-1.0, -1.0, -1.0,
+		1.0, -1.0, -1.0,
+		-1.0, -1.0, 1.0,
+		1.0, -1.0, -1.0,
+		1.0, -1.0, 1.0,
+		-1.0, -1.0, 1.0,
+
+		// Top
+		-1.0, 1.0, -1.0,
+		-1.0, 1.0, 1.0,
+		1.0, 1.0, -1.0,
+		1.0, 1.0, -1.0,
+		-1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0,
+
+		// Front
+		-1.0, -1.0, 1.0,
+		1.0, -1.0, 1.0,
+		-1.0, 1.0, 1.0,
+		1.0, -1.0, 1.0,
+		1.0, 1.0, 1.0,
+		-1.0, 1.0, 1.0,
+
+		// Back
+		-1.0, -1.0, -1.0,
+		-1.0, 1.0, -1.0,
+		1.0, -1.0, -1.0,
+		1.0, -1.0, -1.0,
+		-1.0, 1.0, -1.0,
+		1.0, 1.0, -1.0,
+
+		// Left
+		-1.0, -1.0, 1.0,
+		-1.0, 1.0, -1.0,
+		-1.0, -1.0, -1.0,
+		-1.0, -1.0, 1.0,
+		-1.0, 1.0, 1.0,
+		-1.0, 1.0, -1.0,
+
+		// Right
+		1.0, -1.0, 1.0,
+		1.0, -1.0, -1.0,
+		1.0, 1.0, -1.0,
+		1.0, -1.0, 1.0,
+		1.0, 1.0, -1.0,
+		1.0, 1.0, 1.0,
+	}
+
+	// VAO - Gen and Bind
+	var array uint32
+	gl.GenVertexArrays(1, &array)
+	gl.BindVertexArray(array)
+
+	// Buffers: Gen, Bind, VertexAttribPointer, EnableVertexAttribArray, BufferData
+	var buffer uint32
+	gl.GenBuffers(1, &buffer)
+	gl.BindBuffer(gl.ARRAY_BUFFER, buffer)
+	gl.VertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, gl.PtrOffset(0))
+	gl.EnableVertexAttribArray(positionLocation)
+	gl.BufferData(gl.ARRAY_BUFFER, len(cube)*4, unsafe.Pointer(&cube[0]), gl.STATIC_DRAW)
+
 	// Pre Draw Setup
 	gl.ClearColor(0, 0, 0.2, 1.0)
+	gl.Enable(gl.DEPTH_TEST)
+	gl.DepthFunc(gl.LESS)
 
 	// Main Draw Loop
 	for !win.ShouldClose() {
@@ -71,8 +110,8 @@ func main() {
 		gl.UniformMatrix4fv(viewLocation, 1, false, &view[0])
 
 		// Clear, Draw, Swap, Poll
-		gl.Clear(gl.COLOR_BUFFER_BIT)
-		gl.DrawArrays(gl.TRIANGLES, 0, int32(len(vertices)))
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+		gl.DrawArrays(gl.TRIANGLES, 0, int32(len(cube)))
 		win.SwapBuffers()
 		glfw.PollEvents()
 	}
--- cmd/00-Cube1000Colours/main.go
+++ cmd/00-Cube1000Colours/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"math/rand"
-	"runtime"
-	"time"
 	"unsafe"
 
 	"github.com/go-gl/gl/v4.6-core/gl"
@@ -15,60 +12,44 @@ import (
 
 func main() {
 
-	// Lock this calling goroutine to its current operating system thread.
-	runtime.LockOSThread()
-
-	// Initializes the GLFW library
-	if err := glfw.Init(); err != nil {
-		panic(fmt.Errorf("I could not initialize glfw: %v", err))
-	}
-
-	win, cam := utils.FullScreen()
+	// Window, Camera
+	win, cam := utils.GetWindowAndCamera()
 	defer win.Destroy()
 
-	// Write the graphics card name
-	fmt.Println(utils.GraphicsCardName())
-	// Print instructions
-	fmt.Println("Use W,A,S,D,E,C to move forward, left, backward, right, up and down.")
-
-	utils.SetWASDCallback(win, cam)
-	utils.SetPitchYawCallback(win, cam)
-
-	// Create and Use the Shader
-	program, _ := utils.CreateVF(utils.MVPColourVertShader, utils.MVPColourFragShader)
+	// Program
+	program, _ := utils.CreateVF(utils.ReadShader("colourMVP.vs.glsl"), utils.ReadShader("colourMVP.fs.glsl"))
 	defer gl.DeleteProgram(program)
 	gl.UseProgram(program)
 
-	// Get Uniform and Atrribute Locations
+	// Get Locations
 	modelLocation := gl.GetUniformLocation(program, gl.Str("model\x00"))
 	viewLocation := gl.GetUniformLocation(program, gl.Str("view\x00"))
 	projectionLocation := gl.GetUniformLocation(program, gl.Str("projection\x00"))
 	positionLocation := uint32(gl.GetAttribLocation(program, gl.Str("position\x00")))
 	colourLocation := uint32(gl.GetAttribLocation(program, gl.Str("colour\x00")))
 
-	// Set Model
+	// Bind Locations
 	model := mgl32.Ident4()
 	gl.UniformMatrix4fv(modelLocation, 1, false, &model[0])
-
-	// Set Projection
 	projection := mgl32.Perspective(cam.Fovy, cam.Aspect, cam.Near, cam.Far)
 	gl.UniformMatrix4fv(projectionLocation, 1, false, &projection[0])
 
-	cubeSlice := make([]float32, 0)
-	colourSlice := make([]float32, 0)
-	rand.Seed(time.Now().UTC().UnixNano())
+	// The Vertices
+	cubes := make([]float32, 0)
+	colours := make([]float32, 0)
 
-	for cubeZ := 0; cubeZ < 10; cubeZ++ {
-		for cubeY := 0; cubeY < 10; cubeY++ {
-			for cubeX := 0; cubeX < 10; cubeX++ {
+	for z := 0; z < 10; z++ {
+		for y := 0; y < 10; y++ {
+			for x := 0; x < 10; x++ {
 				// Copy the cube (transforming the vertices)
-				for i := 0; i < len(utils.Cube); i += 3 { // Copy cube (2)
+				for i := 0; i < len(utils.Cube); i += 3 {
 					// Shift to the right
 					var x1, y1, z1 float32
-					x1 = utils.Cube[i] + 14*float32(cubeX)
-					y1 = utils.Cube[i+1] + 14*float32(cubeY)
-					z1 = utils.Cube[i+2] - 14*float32(cubeZ)
-					cubeSlice = append(cubeSlice, x1, y1, z1)
+					x1 = utils.Cube[i] + 14*float32(x)
+					y1 = utils.Cube[i+1] + 14*float32(y)
+					z1 = utils.Cube[i+2] - 14*float32(z)
+					cubes = append(cubes, x1, y1, z1)
+
 				}
 
 				r := rand.Float32()
@@ -76,26 +57,26 @@ func main() {
 				b := rand.Float32()
 
 				// Different Coloured Cube
-				for i := 0; i < len(utils.CubeColour); i += 3 { // Copy cube (2)
+				for i := 0; i < len(utils.Cube); i += 3 { // Copy cube (2)
 
-					colourSlice = append(colourSlice, r, g, b)
+					colours = append(colours, r, g, b)
 				}
-
 			}
 		}
 	}
-	// Create a Vertex Array - I will use 2 buffers, on for vertices, one for colours
+
+	// VAO - Gen and Bind
 	var array uint32
 	gl.GenVertexArrays(1, &array)
 	gl.BindVertexArray(array)
 
-	// Create a Buffer for the vertices
+	// Buffers: Gen, Bind, VertexAttribPointer, EnableVertexAttribArray, BufferData
 	var vertexBuffer uint32
 	gl.GenBuffers(1, &vertexBuffer)
 	gl.BindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
 	gl.VertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, gl.PtrOffset(0))
 	gl.EnableVertexAttribArray(positionLocation)
-	gl.BufferData(gl.ARRAY_BUFFER, len(cubeSlice)*4, unsafe.Pointer(&cubeSlice[0]), gl.STATIC_DRAW)
+	gl.BufferData(gl.ARRAY_BUFFER, len(cubes)*4, unsafe.Pointer(&cubes[0]), gl.STATIC_DRAW)
 
 	// Create a Buffer for the colours
 	var colourBuffer uint32
@@ -104,40 +85,24 @@ func main() {
 	gl.VertexAttribPointer(colourLocation, 3, gl.FLOAT, false, 0, gl.PtrOffset(0))
 	gl.EnableVertexAttribArray(colourLocation)
 	//gl.BufferData(gl.ARRAY_BUFFER, len(utils.CubeColour)*4, unsafe.Pointer(&utils.CubeColour[0]), gl.STATIC_DRAW)
-	gl.BufferData(gl.ARRAY_BUFFER, len(colourSlice)*4, unsafe.Pointer(&colourSlice[0]), gl.STATIC_DRAW)
+	gl.BufferData(gl.ARRAY_BUFFER, len(colours)*4, unsafe.Pointer(&colours[0]), gl.STATIC_DRAW)
 
-	// Set Clear Colour
-	gl.ClearColor(0, 0, 0, 1.0)
-
-	// Set PointSize and/or LineWidth as required
-	//gl.PointSize(4)
-	gl.LineWidth(1)
-
-	// Depth Test (if required)
+	// Pre Draw Setup
+	gl.ClearColor(0, 0, 0.2, 1.0)
 	gl.Enable(gl.DEPTH_TEST)
 	gl.DepthFunc(gl.LESS)
 
-	// // Copy the Geometry to the Array Buffer
-	// fmt.Println("len(cube)", len(utils.Cube))
-	// gl.BufferData(gl.ARRAY_BUFFER, len(utils.Cube)*4, unsafe.Pointer(&utils.Cube[0]), gl.STATIC_DRAW)
-
-	// The Render Loop
+	// Main Draw Loop
 	for !win.ShouldClose() {
 
 		// Update the View Transform, because the Camera may have moved
 		view := mgl32.LookAtV(cam.Position, cam.Position.Add(cam.Forward), cam.Up)
 		gl.UniformMatrix4fv(viewLocation, 1, false, &view[0])
 
-		// Set Clear gl.COLOR_BUFFER_BIT and gl.DEPTH_BUFFER_BIT as required
+		// Clear, Draw, Swap, Poll
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-
-		// Draw the Geometry
-		gl.DrawArrays(gl.TRIANGLES, 0, int32(len(cubeSlice)/3))
-
-		// Swap
+		gl.DrawArrays(gl.TRIANGLES, 0, int32(len(cubes)))
 		win.SwapBuffers()
-
-		// Poll
 		glfw.PollEvents()
 	}
 }
