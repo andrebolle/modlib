3972f8afcbaa4986ff2dadbeb5b8ee789f73e590
Andre Bolle <andrebolle@gmail.com>
2020-04-25 00:51:54 +0100

    update

--- cmd/Basics/07-Next/main.go
+++ cmd/Basics/07-Next/main.go
@@ -15,29 +15,6 @@ import (
 	"github.com/purelazy/modlib/internal/utils"
 )
 
-func loadCubemap(faces []string) uint32 {
-	var textureID uint32
-	gl.GenTextures(1, &textureID)
-
-	gl.BindTexture(gl.TEXTURE_CUBE_MAP, textureID)
-
-	// int width, height, nrChannels;
-	for i := 0; i < len(faces); i++ {
-		rgba := utils.LoadRGBA(faces[i])
-
-		gl.TexImage2D(uint32(gl.TEXTURE_CUBE_MAP_POSITIVE_X+i), 0, gl.RGB, int32(rgba.Rect.Size().X),
-			int32(rgba.Rect.Size().Y), 0, gl.RGBA, gl.UNSIGNED_BYTE, unsafe.Pointer(&rgba.Pix[0]))
-
-	}
-	gl.TexParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
-	gl.TexParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
-	gl.TexParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
-	gl.TexParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-	gl.TexParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_R, gl.CLAMP_TO_EDGE)
-
-	return textureID
-}
-
 func main() {
 
 	// We need an OpenGL context, a window and a camera
